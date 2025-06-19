fe61c8a60cf269a9e4a1052097057bd63608e2ca
anujkanojia29 <anujkanojia@gmail.com>
2025-06-19 09:10:05 +0530

    Added media manager in editor

--- packages/video-editor/src/index.ts
+++ packages/video-editor/src/index.ts
@@ -1,5 +1,9 @@
 import VideoEditor from "./components/video-editor";
 import { usePlayerControl } from "./hooks/use-player-control";
+import BrowserMediaManager from "./helpers/media-manager/browser-media-manager";
+import { MediaItem, PaginationOptions, SearchOptions } from "./helpers/types";
+import BaseMediaManager from "./helpers/media-manager/base-media-manager";
 
-export { usePlayerControl };
+export type { MediaItem, PaginationOptions, SearchOptions };
+export { usePlayerControl, BrowserMediaManager, BaseMediaManager };
 export default VideoEditor;
