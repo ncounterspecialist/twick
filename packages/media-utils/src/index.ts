7d54a06830c269315346331e3f8365829bfb05cd
anujkanojia29 <anujkanojia@gmail.com>
2025-08-13 17:18:42 +0530

    Added audio util and other related changes

--- packages/media-utils/src/index.ts
+++ packages/media-utils/src/index.ts
@@ -6,6 +6,7 @@ export { getAudioDuration } from "./get-audio-duration";
 export { getImageDimensions } from "./get-image-dimensions";
 export { getVideoMeta } from "./get-video-metadata";
 export { getThumbnail } from "./get-thumbnail";
+export { extractAudio, stitchAudio, type AudioSegment } from "./audio-utils";
 export { getObjectFitSize, getScaledDimensions } from "./dimension-handler";
 export { downloadFile, saveAsFile, blobUrlToFile } from "./file-helper";
 export { limit } from "./limit";
